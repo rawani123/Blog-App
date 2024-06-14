86805dbdcbebed91c8a9d8bb3f480fe4fc6a259a
Aniruddha Rawool <aniruddha.212074101@vcet.edu.in>
2024-06-14 10:13:26 +0530

    redux toolkit done

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,29 +1,58 @@
 import React, { useState } from "react";
-import { Box, AppBar, Toolbar, Button, Typography, Tabs, Tab } from "@mui/material";
+import {
+  Box,
+  AppBar,
+  Toolbar,
+  Button,
+  Typography,
+  Tabs,
+  Tab,
+} from "@mui/material";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
-  const [value,setValue] = useState()
+  const isLogin = useSelector((state) => state.isLogin);
+  const [value, setValue] = useState();
   return (
     <>
       <AppBar position="sticky">
         <Toolbar>
-            <Typography variant="h4">
-                My Blog App
-            </Typography>
-            <Box display={'flex'} marginLeft={'auto'} marginRight={'auto'}>
-              <Tabs textColor="inherit" value={value} onChange={(e,val)=> setValue(val)}>
-                <Tab label="Blogs" LinkComponent={Link} to="/"/>
-                <Tab label="My Blogs" LinkComponent={Link} to="/my-blogs"/>
-                <Tab/>
+          <Typography variant="h4">My Blog App</Typography>
+          {isLogin && (
+            <Box display={"flex"} marginLeft={"auto"} marginRight={"auto"}>
+              <Tabs
+                textColor="inherit"
+                value={value}
+                onChange={(e, val) => setValue(val)}
+              >
+                <Tab label="Blogs" LinkComponent={Link} to="/" />
+                <Tab label="My Blogs" LinkComponent={Link} to="/my-blogs" />
+                <Tab />
               </Tabs>
-               
             </Box>
-            <Box display={'flex'} marginLeft={'auto'}>
-                <Button sx={{margin:1,color:'white'}} LinkComponent={Link} to="/login">Login</Button>
-                <Button sx={{margin:1,color:'white'}} LinkComponent={Link} to="/register">Register</Button>
-                <Button sx={{margin:1,color:'white'}}>Logout</Button>
-            </Box>
+          )}
+          <Box display={"flex"} marginLeft={"auto"}>
+            {!isLogin && (
+              <>
+                <Button
+                  sx={{ margin: 1, color: "white" }}
+                  LinkComponent={Link}
+                  to="/login"
+                >
+                  Login
+                </Button>
+                <Button
+                  sx={{ margin: 1, color: "white" }}
+                  LinkComponent={Link}
+                  to="/register"
+                >
+                  Register
+                </Button>
+              </>
+            )}
+            <Button sx={{ margin: 1, color: "white" }}>Logout</Button>
+          </Box>
         </Toolbar>
       </AppBar>
     </>
